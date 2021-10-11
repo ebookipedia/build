62a6e8b80080e7681a7463ee9f352cb7cfb814e3
Santiago Ruiz <hiebra@hotmail.com>
2021-10-11 18:42:17 +0200

    fix

--- src/main/java/mirror/Wikipedia.java
+++ src/main/java/mirror/Wikipedia.java
@@ -1,11 +1,14 @@
 package mirror;
 
+import java.io.File;
+
+import environment.Set;
+
 public class Wikipedia {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("value of args[" + i + "]: " + args[i]);
-		}
+		System.out.println(System.getProperty("user.dir"));
+		Set.variable("empty", String.valueOf(new File("epub").exists()));
 	}
 
 }
