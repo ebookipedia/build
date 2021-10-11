37278474f824bd0e95e9d22ce1948a8125981c4a
Santiago Ruiz <hiebra@hotmail.com>
2021-10-11 11:56:49 +0200

    A test

--- src/main/java/mirror/Wikipedia.java
+++ src/main/java/mirror/Wikipedia.java
@@ -1,12 +1,8 @@
 package mirror;
 
-import org.kohsuke.github.GHEventPayload.Repository;
-
 public class Wikipedia {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println(new Repository());
 		if (args.length > 0) {
 			System.out.println(args[0]);
 		}
