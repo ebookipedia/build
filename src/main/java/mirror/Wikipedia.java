8fc85db7ad26a1073d841bafbe20bfe27f1e0dd4
Santiago Ruiz <hiebra@hotmail.com>
2021-10-11 12:28:50 +0200

    test

--- src/main/java/mirror/Wikipedia.java
+++ src/main/java/mirror/Wikipedia.java
@@ -3,8 +3,8 @@ package mirror;
 public class Wikipedia {
 
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			System.out.println(args[0]);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("value of the parameter #" + i + ": " + args[i]);
 		}
 	}
 
