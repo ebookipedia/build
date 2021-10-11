4a6431feac1c380b294b3bbcaa50eebb165852fc
Santiago Ruiz <hiebra@hotmail.com>
2021-10-11 19:31:35 +0200

    fix

--- src/main/java/mirror/Wikipedia.java
+++ src/main/java/mirror/Wikipedia.java
@@ -9,9 +9,9 @@ public class Wikipedia {
 	public static void main(String[] args) {
 		System.out.println("user.dir: " + System.getProperty("user.dir"));
 		File file = new File("source.epub");
-		boolean exists = file.exists();
-		System.out.println("exists: " + exists);
-		Set.variable("starting", String.valueOf(!exists));
+		boolean starting = !file.exists();
+		System.out.println("starting: " + starting);
+		Set.variable("starting", starting);
 	}
 
 }
