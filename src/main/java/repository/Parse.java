ecb23f7f5eb08f200e5571ad64d1fff75109ad57
Santiago Ruiz <hiebra@hotmail.com>
2021-10-12 17:18:57 +0200

    fix

--- src/main/java/repository/Parse.java
+++ src/main/java/repository/Parse.java
@@ -5,6 +5,15 @@ import com.softalks.github.Repository;
 
 public class Parse {
 
+	private Parse() {
+		// SONAR mandatory
+	}
+
+	/**
+	 * @param args An array with one only item having the value of the
+	 *             GITHUB_REPOSITORY variable
+	 */
+
 	public static void main(String[] args) {
 		Repository repository = new Repository(args[0]);
 		String owner = repository.owner;
